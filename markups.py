90c62fc9630335f1b01188de8890d9fbaa662fac
Gleb Mikhalev <gleb1@bk.ru>
2023-01-24 00:55:13 +0300

    Add 'cancel' markup for change_mycity

--- markups.py
+++ markups.py
@@ -2,9 +2,16 @@ from telebot import types
 
 
 def reply_markup():
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, selective=True)
     button1 = types.KeyboardButton('Погода в моем городе')
     button2 = types.KeyboardButton('Изменить мой город')
     button3 = types.KeyboardButton('Помощь')
     markup.add(button1, button2, button3)
+    return markup
+
+
+def reply_markup_change_mycity():
+    markup = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton(text='Отменить изменение', callback_data='cancel')
+    markup.add(button)
     return markup
