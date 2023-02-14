810b778fcd2f3e1babff8efec798f37a36b69c43
Gleb Mikhalev <gleb1@bk.ru>
2023-02-14 18:18:31 +0300

    Add data base for users default cities

--- change_mycity_operation.py
+++ change_mycity_operation.py
@@ -1,4 +1,5 @@
 import telebot
+from collections import defaultdict
 from decouple import config
 from markups import Markups
 from commands import Commands
@@ -9,34 +10,38 @@ bot = telebot.TeleBot(config('telegram_token'), parse_mode='html')
 
 
 class MyCity:
-    default_city = 'Москва'
+    users_default_cities = defaultdict(lambda: "Москва")
     check_change_mycity = False
 
-    @staticmethod
-    def prepare_change_mycity(chat_id):
-        MyCity.check_change_mycity = True
+    @classmethod
+    def prepare_change_mycity(cls, chat_id, user_id):
+        cls.check_change_mycity = True
         bot.send_message(chat_id=chat_id,
-                         text=Commands.change(MyCity.default_city),
+                         text=Commands.change(cls.users_default_cities[user_id]),
                          reply_markup=Markups.cancel())
 
-    @staticmethod
-    def change_mycity(message):
+    @classmethod
+    def change_mycity(cls, message):
         city = message.text
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        default_city = cls.users_default_cities[user_id]
         response = get_weather(city)
 
         if not response['status']:
-            bot.send_message(chat_id=message.chat.id,
+            bot.send_message(chat_id=chat_id,
                              text=f"Город <b>{city}</b> не найден!\n"
                                   "Попробуйте ввести другой",
                              reply_markup=Markups.cancel())
-        elif MyCity.default_city == response['city']:
-            bot.send_message(chat_id=message.chat.id,
+        elif default_city == response['city']:
+            bot.send_message(chat_id=chat_id,
                              text=f"Данный город уже назначен!\n"
                                   "Попробуйте ввести другой",
                              reply_markup=Markups.cancel())
         else:
-            MyCity.default_city = city
-            MyCity.check_change_mycity = False
-            bot.send_message(chat_id=message.chat.id,
-                             text=f"Ваш город изменен на <b>{MyCity.default_city}</b>",
+            cls.users_default_cities[user_id] = city
+            cls.check_change_mycity = False
+            print(cls.users_default_cities)
+            bot.send_message(chat_id=chat_id,
+                             text=f"Ваш город изменен на <b>{city}</b>",
                              reply_markup=Markups.plates())
--- weather_bot.py
+++ weather_bot.py
@@ -12,69 +12,84 @@ bot = telebot.TeleBot(config('telegram_token'), parse_mode='html')
 @bot.message_handler(func=lambda message: message.text[0] == '/')
 def check_commands(message):
     text = message.text
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+
     if text == '/start':
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.start(message.from_user.first_name,
-                                             message.from_user.username),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.start(name=message.from_user.first_name,
+                                             username=message.from_user.username),
                          reply_markup=Markups.plates())
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.first_change(MyCity.default_city),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.first_change(default_city=MyCity.users_default_cities[user_id]),
                          reply_markup=Markups.start_change())
     elif text == '/help':
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.help(MyCity.default_city),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.help(default_city=MyCity.users_default_cities[user_id]),
                          reply_markup=Markups.plates())
     elif text == '/mycity':
-        bot.send_chat_action(chat_id=message.chat.id, action='typing')
-        get_city(message.chat.id, Commands.mycity(MyCity.default_city))
+        bot.send_chat_action(chat_id=chat_id, action='typing')
+        get_city(chat_id=chat_id,
+                 city=MyCity.users_default_cities[user_id])
     elif text == '/change_mycity':
-        MyCity.prepare_change_mycity(message.chat.id)
+        MyCity.prepare_change_mycity(chat_id=chat_id,
+                                     user_id=user_id)
     else:
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.other(message.text),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.other(text=message.text),
                          reply_markup=Markups.plates())
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    chat_id = call.from_user.id
+    user_id = chat_id
+
     if call.data == 'cancel':
         MyCity.check_change_mycity = False
-        bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id)
-        bot.send_message(chat_id=call.from_user.id,
+        bot.delete_message(chat_id=chat_id, message_id=call.message.message_id)
+        bot.send_message(chat_id=chat_id,
                          text="Изменения отменены",
                          reply_markup=Markups.plates())
         # bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id-1)
         # bot.answer_callback_query(call.id, text="Изменения отменены")
     elif call.data == 'yes':
-        MyCity.prepare_change_mycity(call.from_user.id)
+        MyCity.prepare_change_mycity(chat_id=chat_id, user_id=user_id)
     elif call.data == 'no':
-        bot.send_message(chat_id=call.from_user.id,
-                         text=f"Ваш город сохранен как <b>{MyCity.default_city}</b>")
+        bot.send_message(chat_id=chat_id,
+                         text=f"Ваш город сохранен как <b>{MyCity.users_default_cities[user_id]}</b>")
 
 
 @bot.message_handler(content_types=['text'])
 def check_user_text(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    default_city = MyCity.users_default_cities[user_id]
+
     if MyCity.check_change_mycity:
-        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_chat_action(chat_id=chat_id,
+                             action='typing')
         MyCity.change_mycity(message)
         return
 
-    text = message.text.lower()
     if text == 'погода в моем городе':
-        bot.send_chat_action(message.chat.id, 'typing')
-        get_city(message.chat.id, Commands.mycity(MyCity.default_city))
+        bot.send_chat_action(chat_id=chat_id,
+                             action='typing')
+        get_city(message.chat.id, Commands.mycity(default_city=default_city))
     elif text == 'изменить мой город':
         MyCity.check_change_mycity = True
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.change(MyCity.default_city),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.change(default_city=default_city),
                          reply_markup=Markups.cancel())
     elif text == 'помощь':
-        bot.send_message(chat_id=message.chat.id,
-                         text=Commands.help(MyCity.default_city),
+        bot.send_message(chat_id=chat_id,
+                         text=Commands.help(default_city=default_city),
                          reply_markup=Markups.plates())
     else:
-        bot.send_chat_action(message.chat.id, 'typing')
-        get_city(message.chat.id, message.text)
+        bot.send_chat_action(chat_id=chat_id,
+                             action='typing')
+        get_city(chat_id=message.chat.id, city=message.text)
 
 
 bot.infinity_polling()
